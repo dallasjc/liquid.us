fdae865870ba8e74a96f2fe5ecffd2fcf70be165
dallasjc <39286778+dallasjc@users.noreply.github.com>
2019-07-23 11:35:49 -0500

    Added form to sign up and ask questions

--- views/endorsement-questions.js
+++ views/endorsement-questions.js
@@ -12,7 +12,7 @@ module.exports = (state, dispatch) => {
       style=${{ marginTop: '-.5em' }}
     >
       ${askQuestionButton(state, dispatch)}
-      ${vote.showQuestionForm ? questionFormRegistered(state, dispatch) : ''}
+      ${vote.showQuestionForm && state.user ? questionFormRegistered(state, dispatch) : vote.showQuestionForm ? questionFromSignupForm(state, dispatch) : ''}
       ${loading.questions ? activityIndicator({ size: 'small' }) : questions(state, dispatch)}
     </div>
   `
@@ -26,11 +26,10 @@ const askQuestionButton = (state, dispatch) => {
     <div class="level" style=${{ paddingBottom: '1em' }}>
       <div class="level-item">
         <button
-          onclick=${(event) => state.user && dispatch({ type: 'vote:questionFormActivated', vote, event })}
+          onclick=${(event) => dispatch({ type: 'vote:questionFormActivated', vote, event })}
           class="button is-primary has-text-weight-semibold is-small"
-          disabled=${!state.user}
         >
-          <span>${state.user ? `Ask a question` : `Endorse or sign up to ask a question`}</span>
+          <span>Ask a question</span>
         </button>
       </div>
     </div>
@@ -87,6 +86,84 @@ const questionFormRegistered = (state, dispatch) => {
   `
 }
 
+const questionFromSignupForm = (state, dispatch) => {
+  const { error, loading, location, user, votes } = state
+  const vote = votes[location.params.voteId]
+
+  return html`
+    <form
+      method="POST"
+      style="margin-bottom: 2rem;"
+      onconnected=${scrollToForm(location)}
+      onsubmit=${handleForm(dispatch, { type: 'vote:questionFromSignupForm', vote })}
+    >
+      <div class="field">
+        <h4 class="title is-size-5">Ask a question</h4>
+      </div>
+      ${error ? html`<div class="notification is-warning">${error.message}</div>` : ''}
+      <input type="hidden" value="${vote.id}" name="vote_id" />
+      <div class="${user ? 'columns is-hidden' : 'columns'}">
+        <div class="column">
+          <div class="field">
+            <label class="label has-text-grey">Your Name *</label>
+            <div class="control has-icons-left">
+              <input name="name" class="input" type="text" placeholder="John Doe" />
+              <span class="icon is-small is-left"><i class="fa fa-user"></i></span>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="field">
+            <label class="label has-text-grey">Your Email *</label>
+            <div class="field join-input-field">
+              <div class="control has-icons-left">
+                <input name="email" class="input" type="text" placeholder="you@example.com" />
+                <span class="icon is-small is-left"><i class="fa fa-user"></i></span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="${user ? 'field is-hidden' : 'field'}">
+        <label class="label has-text-grey">Your Address</label>
+        <div class="control has-icons-left">
+          <input onconnected=${initGoogleMaps} id="address_autocomplete_sidebar" class="input" autocomplete="off" name="address" placeholder="185 Berry Street, San Francisco, CA 94121" />
+          <span class="icon is-small is-left"><i class="fa fa-map-marker-alt"></i></span>
+        </div>
+        <p class="is-size-7" style="margin-top: .3rem;">So we can connect you to your reps and fellow constituents.</p>
+      </div>
+      <div class="field">
+        <label class="${user ? 'field is-hidden' : 'label has-text-grey'}">Your Question</label>
+        <div class="control">
+          <textarea name="question" autocomplete="off" class="textarea" placeholder="What do you want to know?"></textarea>
+        </div>
+      </div>
+      <div class="field">
+        <div class="control">
+          <label class="checkbox">
+            <input name="public" type="checkbox" checked=${user ? user.last_vote_public : true} />
+            Share my name publicly
+          </label>
+        </div>
+      </div>
+      <div class="field is-horizontal">
+        <div class="field is-grouped">
+          <div class="control">
+            <button
+              class=${`button ${loading.questions ? 'is-loading' : ''}`}
+              disable=${loading.questions}
+              type="submit"
+            >
+              <span class="icon"><i class="fa fa-edit"></i></span>
+              <span>Submit</span>
+            </button>
+          </div>
+        </div>
+      </div>
+      <hr />
+    </form>
+  `
+}
 const questions = (state, dispatch) => {
   const { location, votes } = state
   const vote = votes[location.params.voteId]
@@ -137,3 +214,4 @@ const scrollToForm = (location) => {
     window.scrollTo(0, document.getElementById('measure-vote-form').getBoundingClientRect().top)
   }
 }
+const initGoogleMaps = (event) => window.initGoogleAddressAutocomplete(event.currentTarget.getAttribute('id'))

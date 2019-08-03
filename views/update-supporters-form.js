5822c8fcb8b04b54d501c5ea2dc8351f5b32d5ac
dallasjc <39286778+dallasjc@users.noreply.github.com>
2019-08-03 10:15:00 -0500

    Add dropdown

--- views/update-supporters-form.js
+++ views/update-supporters-form.js
@@ -1,10 +1,9 @@
-const { WWW_URL } = process.env
 const { handleForm, html } = require('../helpers')
 
 module.exports = (state, dispatch) => {
   const { forms, loading, location, updates = {} } = state
   const update = updates[location.params.id] || {}
-  const { measure, recipients, subject, body } = update
+  const { subject, body } = update
   const form = forms.editUpdate || {}
 
 
@@ -25,13 +24,62 @@ module.exports = (state, dispatch) => {
               </div>
               <div class="columns">
                 <div class="column">
-                  <div class="field">
-                    <b>Body text</b>
-                    <div class="control" style="padding-top:0.3rem;">
-                      <textarea name="body" autocomplete="off" class="textarea" rows="10" required value="${body || 'Dear $ {first_name},'}"></textarea>
-                      <p class="help">You can continue to edit your update later.</p>
+                  <div class="label">Body text</div>
+                </div>
+                <div class="column is-narrow-is-right">
+                  <p>Insert variable</p>
+                </div>
+                <div class="column is-narrow-is-right">
+                  <div class="dropdown is-active">
+                    <div class="dropdown-trigger">
+                      <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
+                        <span>Select</span>
+                        <span class="icon is-small">
+                          <i class="fas fa-angle-down" aria-hidden="true"></i>
+                        </span>
+                      </button>
+                    </div>
+                    <div class="dropdown-menu" id="dropdown-menu" role="menu">
+                      <div class="dropdown-content">
+                        <a href="#" class="dropdown-item">
+                        Recipient First Name
+                        </a>
+                        <a href="#" class="dropdown-item">
+                        Rep First Name
+                        </a>
+                        <a href="#" class="dropdown-item is-active">
+                          Rep Last Name
+                        </a>
+                        <a href="#" class="dropdown-item">
+                          Rep Email
+                        </a>
+                        <a href="#" class="dropdown-item">
+                          Rep Phone Number
+                        </a>
+                        <a href="#" class="dropdown-item">
+                          Rep District
+                        </a>
+                        <a href="#" class="dropdown-item">
+                          Senator First Name
+                        </a>
+                        <a href="#" class="dropdown-item">
+                          Senator Last Name
+                        </a>
+                        <a href="#" class="dropdown-item">
+                          Senate District
+                        </a>
+                        <a href="#" class="dropdown-item">
+                          Senator Phone
+                        </a>
+                      </div>
                     </div>
                   </div>
+                </div>
+              </div>
+              <div class="field">
+                <div class="control" style="padding-top:0.3rem;">
+                  <textarea name="body" autocomplete="off" class="textarea" rows="10" required value="${body || 'Dear $ {first_name},'}"></textarea>
+                  <p class="help">You can continue to edit your update later.</p>
                 </div>
               </div>
               <div class="field is-grouped">
@@ -49,7 +97,6 @@ module.exports = (state, dispatch) => {
             </div>
             <div class="column is-size-5">
               <div class="card" style="margin:1rem; padding:1rem;">
-
                 <p><i>Save draft language to populate subject</i></p><br />
                 <p><b>From:</b> Hold Charter Schools Accountable via Liquid US</p><br />
                 <p>Dear Dallas,</p>
